5b8a7a82de56572884b38d87ffe863cb8f0b47b2
Ariel <ariel.von.barnekow@upcnet.es>
2011-10-06 13:54:56 +0200

    Traduccions de Collage, Poi, windowZ. I modificat l'script per a que fer l'enllaç

--- linktranslations.sh
+++ linktranslations.sh
@@ -1,7 +1,46 @@
 #!/bin/bash
-
 echo  'NOTA: Si heu actualitzat versions de paquets, cal modificar aquest arxiu.'
 
+#Directori on estan les traduccions
+TRANS=`pwd`
+# BUILD tindria que ser el directori on esta el gw4, el valor seguent es la ruta relativa sense barra al final
+BUILD=${TRANS//\/src\/translations/}
+
+SOURCES=( )
+
+DESTINATIONS=( )
+
+function add {
+    SOURCES[${#SOURCES[*]}]="$1"
+    DESTINATIONS[${#DESTINATIONS[*]}]="$2"
+}
+
+
+
+# Collage
+add '/Collage/locales/es/LC_MESSAGES/*' '/src/Products.Collage/Products/Collage/locales/es/LC_MESSAGES/'
+add '/Collage/locales/ca/LC_MESSAGES/*' '/src/Products.Collage/Products/Collage/locales/ca/LC_MESSAGES/'
+
+# Poi
+add '/Poi/locales/ca/' '/src/Products.Poi/Products/Poi/locales/ca/'
+add '/Poi/locales/es/' '/src/Products.Poi/Products/Poi/locales/es/'
+
+# WindowZ
+add '/windowZ/locales/' '/src/Products.windowZ/Products/windowZ/'
+# WindowZ no te locales al configure el sobreescribim
+add '/windowZ/configure.zcml' '/src/Products.windowZ/Products/windowZ/' 
+
+
+
+i=0
+
+for s in ${SOURCES[@]}; do
+    
+    #echo $TRANS$s $BUILD${DESTINATIONS[$i]}
+    ln -s -f $TRANS$s $BUILD${DESTINATIONS[$i]}
+    i=$(( $i + 1 ))
+
+done
 
 
 
