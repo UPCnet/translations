999e1fdc2881093eedac5c1a847a5629231440fd
Ariel <ariel.von.barnekow@upcnet.es>
2012-01-13 11:19:45 +0100

    Link transaltion amb suport per mode desenvolupador (-d) / producció

--- linktranslations.sh
+++ linktranslations.sh
@@ -1,76 +1,104 @@
 #!/bin/bash
+############################################################
+# - linktranslations -
+# Sobreescriu traduccions per defecte per propies
+# Usage: linktranslations [-d]
+# Parametres
+#       -d      Mode desenvolupador (sobreescriure paquets src)
+###############################################################
+DEVELOP=false
+
+# Llegim parametres
+if [ "$1" = "-d" ]; then
+  DEVELOP=true
+fi
+
 #Directori on estan les traduccions
 TRANS=`pwd`
+
 # BUILD tindria que ser el directori on esta el gw4, el valor seguent es la ruta relativa sense barra al final
+# La ruta es diferent a desenvolupament que ha producció
+
 BUILD=${TRANS//\/translations/}
 
+if $DEVELOP ; then
+    BUILD=${TRANS//\/src\/translations/}
+fi
 
 SOURCES=( )
 
 DESTINATIONS=( )
 
+# add(override, desti, develop)
+# @override string ruta a la nostra traducció dins de TRANS
+# @desti string ruta on va la traducció
+# @develop true | false Indica si estem a mode develop si s'ha de buscar el
+#                       paquet a egss o a src
 function add {
     SOURCES[${#SOURCES[*]}]="$1"
-    DESTINATIONS[${#DESTINATIONS[*]}]="$2"
+    if $DEVELOP ; then        
+        DESTINATIONS[${#DESTINATIONS[*]}]="/src$2"
+    else
+        DESTINATIONS[${#DESTINATIONS[*]}]="/eggs$2"
+    fi
 }
 
 ############
 # SRC LINKS
 ############
-
+DEVELOP=$DEVELOP
 # Collage
 # Collage ja té locales només actualitzar traduccions
-add '/Collage/locales/es/LC_MESSAGES/*' '/eggs/Products.Collage*/Products/Collage/locales/es/LC_MESSAGES/'
-add '/Collage/locales/ca/LC_MESSAGES/*' '/eggs/Products.Collage*/Products/Collage/locales/ca/LC_MESSAGES/'
-
-#Ploneboard
-add '/PloneBoard/configure.zcml' '/eggs/Products.Ploneboard*/Products/Ploneboard/'
-add '/PloneBoard/locales/' '/eggs/Products.Ploneboard*/Products/Ploneboard/'
+add '/Collage/locales/es/LC_MESSAGES/*' '/Products.Collage*/Products/Collage/locales/es/LC_MESSAGES/'
+add '/Collage/locales/ca/LC_MESSAGES/*' '/Products.Collage*/Products/Collage/locales/ca/LC_MESSAGES/'
 
 #PlonePopoll
-add '/PlonePopoll/configure.zcml' '/eggs/Products.PlonePopoll*/Products/PlonePopoll/'
-add '/PlonePopoll/locales/' '/eggs/Products.PlonePopoll*/Products/PlonePopoll/'
+add '/PlonePopoll/configure.zcml' '/Products.PlonePopoll*/Products/PlonePopoll/' 
+add '/PlonePopoll/locales/' '/Products.PlonePopoll*/Products/PlonePopoll/'
 
 #PloneSurvey
-add '/PloneSurvey/configure.zcml' '/eggs/Products.PloneSurvey*/Products/PloneSurvey/'
-add '/PloneSurvey/locales/' '/eggs/Products.PloneSurvey*/Products/PloneSurvey/'
+add '/PloneSurvey/configure.zcml' '/Products.PloneSurvey*/Products/PloneSurvey/' $DEVELOP
+add '/PloneSurvey/locales/' '/Products.PloneSurvey*/Products/PloneSurvey/'
 
 # Poi
 # Poi ja té locales només actualitzar traduccions
-add '/Poi/locales/ca/LC_MESSAGES/*' '/eggs/Products.Poi*/Products/Poi/locales/ca/LC_MESSAGES/'
-add '/Poi/locales/es/LC_MESSAGES/*' '/eggs/Products.Poi*/Products/Poi/locales/es/LC_MESSAGES/'
+add '/Poi/locales/ca/LC_MESSAGES/*' '/Products.Poi*/Products/Poi/locales/ca/LC_MESSAGES/'
+add '/Poi/locales/es/LC_MESSAGES/*' '/Products.Poi*/Products/Poi/locales/es/LC_MESSAGES/'
 
 # WindowZ
 # 2011-11-24 Ara esta al pypi upc
-#add '/windowZ/locales/' '/eggs/Products.windowZ/Products/windowZ/'
+#add '/windowZ/locales/' '/Products.windowZ/Products/windowZ/'
 # WindowZ no te locales al configure el sobreescribim
-#add '/windowZ/configure.zcml' '/eggs/Products.windowZ/Products/windowZ/'
+#add '/windowZ/configure.zcml' '/Products.windowZ/Products/windowZ/'
 
 
 ########
 # EGGS
 ########
-
+DEVELOP=false
 # PloneFormGen (desactivat) 
 # a 1.7rc1, tenim les nostres traduccions!!!
 #
 # PloneFormGen no te traducció al català linkem tota la carpeta
-#add '/PloneFormGen/locales/ca/' '/eggs/Products.PloneFormGen*.egg/Products/PloneFormGen/locales/'
+#add '/PloneFormGen/locales/ca/' '/Products.PloneFormGen*.egg/Products/PloneFormGen/locales/'
 # plone form gen si té traducció al español
-#add '/PloneFormGen/locales/es/LC_MESSAGES/*' '/eggs/Products.PloneFormGen*.egg/Products/PloneFormGen/locales/es/LC_MESSAGES/'
+#add '/PloneFormGen/locales/es/LC_MESSAGES/*' '/Products.PloneFormGen*.egg/Products/PloneFormGen/locales/es/LC_MESSAGES/'
+
+#Ploneboard
+add '/PloneBoard/configure.zcml' '/Products.Ploneboard*/Products/Ploneboard/'
+add '/PloneBoard/locales/' '/Products.Ploneboard*/Products/Ploneboard/'
 
 #plone.app.locales
-# Esborrat: al nou paquet està la traducció de la Lidia
 # L'afegim ja que hi han cadenes que sobreeescribim
-add '/plone.app.locales/ca/*' '/eggs/plone.app.locales*.egg/plone/app/locales/locales/ca/LC_MESSAGES/'
-add '/plone.app.locales/es/*' '/eggs/plone.app.locales*.egg/plone/app/locales/locales/es/LC_MESSAGES/'
+add '/plone.app.locales/ca/*' '/plone.app.locales*.egg/plone/app/locales/locales/ca/LC_MESSAGES/'
+add '/plone.app.locales/es/*' '/plone.app.locales*.egg/plone/app/locales/locales/es/LC_MESSAGES/'
 
 #plone.app.discussion
-add '/plone.app.discussion/ca/*' '/eggs/plone.app.discussion*.egg/plone/app/discussion/locales/ca/LC_MESSAGES/'
-add '/plone.app.discussion/es/*' '/eggs/plone.app.discussion*.egg/plone/app/discussion/locales/es/LC_MESSAGES/'
+add '/plone.app.discussion/ca/*' '/plone.app.discussion*.egg/plone/app/discussion/locales/ca/LC_MESSAGES/'
+add '/plone.app.discussion/es/*' '/plone.app.discussion*.egg/plone/app/discussion/locales/es/LC_MESSAGES/'
 
 #Products.CMFCalendar
-add '/CMFCalendar/ca' '/eggs/Products.CMFCalendar*.egg/Products/CMFCalendar/locales/'
+add '/CMFCalendar/ca' '/Products.CMFCalendar*.egg/Products/CMFCalendar/locales/'
 
 i=0
 
